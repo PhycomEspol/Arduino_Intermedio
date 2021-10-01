0de9542c4080ac6291fbc9152e0f9ce433cde1da
PhycomEspol <phycom@espol.edu.ec>
2021-09-30 20:01:47 -0500

    motor dc con cambio de giro

--- 6_Motores/1MotorDC/1MotorDC.ino
+++ 6_Motores/1MotorDC/1MotorDC.ino
@@ -1,3 +1,4 @@
+#define BUTTONCHANGE 6
 #define BUTTONRIGHT 7
 #define BUTTONLEFT 8
 #define IN1 9
@@ -7,15 +8,19 @@
 int speedMotor = 255;
 bool statusLeft;
 bool statusRight;
+bool statusChange;
+bool giro;
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(BUTTONRIGHT, INPUT);
   pinMode(BUTTONLEFT, INPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
- 
+
+  giro = true;
   //encender motor
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
@@ -23,9 +28,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  statusRight = digitalRead(BUTTONRIGHT);
-  statusLeft = digitalRead(BUTTONLEFT);
-  Serial.println(statusRight);
+  statusRight = digitalRead(BUTTONRIGHT);//aumento de velocidad
+  statusLeft = digitalRead(BUTTONLEFT);//disminucion de velocidad
+  statusChange = digitalRead(BUTTONCHANGE);//cambio de giro
+  
   if(statusLeft == HIGH){
     if(speedMotor <= 0){
       speedMotor = 0;
@@ -41,7 +47,21 @@ void loop() {
       speedMotor = speedMotor + 5; 
     }
   }
-
+  
+  if(statusChange == HIGH){//cambio de giro
+    Serial.println("button giro presionado");
+    giro = !giro;
+    if(giro == true){
+      Serial.println("giro true");
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+    }else{
+      Serial.println("giro false");
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+    }
+  }
+  
   analogWrite(ENA, speedMotor);
   delay(150);
 }
